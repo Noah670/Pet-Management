66a7d0c86d5d3cdfdf416ff4069c03a3af9e51a0
Noah <noahrules96950@gmail.com>
2020-03-21 18:42:30 +1000

    Add files via upload
    Sample Cat User for Pet Management Check In

---
+++ Cat.java
@@ -0,0 +1,29 @@
+// Noah Pohl
+// IT-145 SNHU
+// 3-11-20
+// Cat.java
+
+// inherits from the Pet class
+public class Cat extends Pet {
+    
+    // Constructor for Cat class
+    public Cat(String petType, String petName, int petAge, int dogSpace, int catSpace, int daysStay, double amountDue) {
+        super(petType, petName, petAge, dogSpace, catSpace, daysStay, amountDue);
+    
+    }
+
+    // catSpaceNbr method
+    public void catSpaceNbr() {
+
+        getCatSpace();
+        catSpaceNbr();
+    }
+
+    public static void main(String[] args) {
+        Pet myCuteCat = new Pet(null, null, 0, 0, 0, 0, 0);
+        myCuteCat.checkIn();
+    }
+
+   
+
+}
